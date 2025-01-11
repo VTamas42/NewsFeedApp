63effa020e86853abc85610f32a1570a9c6b6402
VinczeTamas <v.tomy18@gmail.com>
2025-01-11 20:12:25 +0100

    Creating news modell and page

---
+++ NewsFeedApp/Models/News.cs
@@ -0,0 +1,14 @@
+﻿namespace NewsFeedApp.Models
+{
+    public class News
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public string Description { get; set; }
+        public DateTime PubDate { get; set; }
+        public string ArcticleLink { get; set; }
+        public string GUID { get; set; }
+        public string MediaContent { get; set; }
+
+    }
+}
